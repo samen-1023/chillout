6aab7f99c12d753288db58dc0e35e5772ec1a009
samen-1023 <semyon.kondratenko@zapomni.ru>
2022-07-16 20:13:49 +0200

    Init server

---
+++ src/configs/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+    // Server
+    api_host: 'localhost',
+    api_port: 5000,
+
+    // Database
+    db_name: 'chillout',
+    db_host: 'localhost',
+    db_port: 8432,
+    db_user: '...',
+    db_password: '...',
+
+    // Remote api url's
+};
---
+++ src/core/index.ts
@@ -0,0 +1,5 @@
+export * from './core.controller';
+export * from './core.entity';
+export * from './core.middleware';
+export * from './core.router';
+export * from './core.usecase';
